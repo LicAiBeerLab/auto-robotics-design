a16799c8393e797f10aa918ef58df4eb83652bc1
Huowl <49783047+Huowl@users.noreply.github.com>
2024-02-21 18:14:51 +0300

    optimizer

--- auto_robot_design/description/mechanism.py
+++ auto_robot_design/description/mechanism.py
@@ -37,6 +37,7 @@ class KinematicGraph(nx.Graph):
         self.main_branch: nx.Graph = nx.Graph()
         self.kinematic_tree: nx.Graph = nx.Graph()
         self.joint_graph: nx.Graph = nx.Graph()
+        self.jps_graph: nx.Graph = nx.Graph()
 
     def define_main_branch(self):
         ground_joints = sorted(
@@ -196,7 +197,7 @@ def JointPoint2KinematicGraph(jp_graph: nx.Graph):
     JP2Joint = {}
     for jp in jp_graph.nodes():
         JP2Joint[jp] = Joint(jp)
-
+    jps_graph = deepcopy(jp_graph)
     joint_graph: nx.Graph = nx.relabel_nodes(jp_graph, JP2Joint)
 
     ground_joints = set([JP2Joint[jp] for jp in get_ground_joints(jp_graph)])
@@ -272,6 +273,7 @@ def JointPoint2KinematicGraph(jp_graph: nx.Graph):
     kin_graph.EE = ee_link
     kin_graph.G = ground_link
     kin_graph.joint_graph = joint_graph
+    kin_graph.jps_graph = jps_graph
     for joint in joint_graph.nodes():
         connected_links = list(joint.links)
         if len(connected_links) == 2:
---
+++ auto_robot_design/optimization/optimizer.py
@@ -0,0 +1,68 @@
+import numpy as np
+import networkx as nx
+
+from cmaes import CMA
+
+def create_dict_jp_limit(joints, limit):
+    jp2limits = {}
+    for jp, lim in zip(joints, limit):
+        jp2limits[jp] = lim
+    return jp2limits
+
+class Optimizer:
+    def __init__(self, graph: nx.Graph, jp2limits, weights, **params_optimizer) -> None:
+        self.graph = graph
+        self.jp2limits = jp2limits
+        self.params_optimizer = params_optimizer
+        self.opt_joints = list(self.jp2limits.keys())
+        self.initial_xopt, __, __, __ = self.convert_joints2x_opt()
+        self.weights = weights
+        self.history = {}
+    def convert_joints2x_opt(self):
+        x_opt = []
+        upper_bounds = []
+        lower_bounds = []
+        for jp in self.opt_joints:
+            lims = self.jp2limits[jp]
+            x_opt += [jp.r[0], jp.r[2]]
+            upper_bounds += list(lims[2:])
+            lower_bounds += list(lims[:2])
+        
+        return x_opt, self.opt_joints, upper_bounds, lower_bounds
+
+    def mutate_JP_by_xopt(self, x_opt):
+        num_params_one_jp = len(x_opt) / len(self.opt_joints)
+        
+        for id, jp in zip(range(0, len(x_opt), num_params_one_jp), self.opt_joints):
+            jp.r = x_opt[id:(id+num_params_one_jp)]
+
+
+    def run(self):
+        ___, opt_joints, upper_bounds, lower_bounds = self.convert_joints2x_opt()
+        
+        bounds = np.array(list(zip(lower_bounds, upper_bounds))).T
+        
+        optimizer_CMA = CMA(mean=self.initial_xopt,bounds=bounds, **self.params_optimizer)
+        
+        for generation in range(self.params_optimizer['generations']):
+            solutions = []
+            for __ in range(self.params_optimizer['population_size']):
+                x = optimizer_CMA.ask()
+                costs = self.get_cost(x)
+                fval = self.calc_fval(costs)
+                solutions.append((x, fval))
+                self.history[x] = (costs, fval)
+            optimizer_CMA.tell(solutions)
+            
+            if optimizer_CMA.should_stop():
+                popsize = optimizer_CMA.population_size * 2
+                self.params_optimizer['population_size'] = popsize
+                mean = np.array(lower_bounds) + (np.random.rand(len(lower_bounds)) * (np.array(upper_bounds) - np.array(lower_bounds)))
+                optimizer_CMA = CMA(mean=mean,bounds=bounds, **self.params_optimizer)
+    
+    def get_cost(self, x):
+        self.mutate_JP_by_xopt(x)
+        pass
+    
+    def calc_fval(self, costs):
+        return np.sum(costs @ self.weights)
