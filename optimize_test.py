607511d37ca8a40bfaf1535cf0d55e59a92694fe
Huowl <49783047+Huowl@users.noreply.github.com>
2024-02-23 12:38:46 +0300

    add save load optimizer

--- optimize_test.py
+++ optimize_test.py
@@ -22,13 +22,31 @@ draw_joint_point(graph)
 plt.show()
 # %% 
 
-from auto_robot_design.optimization.optimizer import Optimizer, create_dict_jp_limit
+from auto_robot_design.optimization.optimizer import Optimizer, jps_graph2urdf
 
 optimizing_joints = dict(filter(lambda x: x[1]["optim"], constrain_dict.items()))
 name2jp = dict(map(lambda x: (x.name, x), graph.nodes()))
-optimizing_joints = dict(map(lambda x: (name2jp[x[0]], (x[1]["x_range"][0], x[1].get("z_range", [0,0])[0], x[1]["x_range"][1], x[1].get("z_range", [0,0])[1])), optimizing_joints.items()))
+optimizing_joints = dict(map(lambda x: (name2jp[x[0]], (x[1]["x_range"][0], x[1].get("z_range", [-0.01,0.01])[0], x[1]["x_range"][1], x[1].get("z_range", [0,0])[1])), optimizing_joints.items()))
 # limits = {j:()}
+optimizer = {}
 
-opt_eng = Optimizer(graph, optimizing_joints, np.ones(1)*1, "test_two_links")
+opt_eng = Optimizer(graph, optimizing_joints, np.array([1, 0.4]), "two_links", **optimizer)
+cost = opt_eng.get_cost(opt_eng.initial_xopt)
+print(cost, opt_eng.calc_fval(cost))
 
-print(opt_eng.get_cost(opt_eng.initial_xopt))
+opt_eng.run(50)
+
+histor_x = sorted(opt_eng.history, key=lambda x: x[2])
+
+print([x[2] for x in histor_x])
+
+best_x = histor_x[0]
+print(best_x[1], best_x[2])
+opt_eng.mutate_JP_by_xopt(best_x[0])
+draw_joint_point(opt_eng.graph)
+plt.show()
+urdf, __, __ = jps_graph2urdf(opt_eng.graph)
+with open("test.urdf", "w") as f:
+    f.write(urdf)
+
+print("success")
