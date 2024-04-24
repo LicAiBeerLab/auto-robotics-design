b34e4c280562951888eae36928aace34046e657c
ZharkovKirill <rubekon13@gmail.com>
2024-04-24 19:23:26 +0300

    661

--- apps/pino_examples/squat_example.py
+++ apps/pino_examples/squat_example.py
@@ -11,6 +11,7 @@ from auto_robot_design.description.utils import (
     all_combinations_active_joints_n_actuator, )
 
 from auto_robot_design.generator.restricted_generator.two_link_generator import TwoLinkGenerator
+from auto_robot_design.pinokla.simulation_compare import max_toque_ratio, сomparison_ratio_positive_torques
 from auto_robot_design.pinokla.squat import SquatHopParameters, SimulateSquatHop
 gen = TwoLinkGenerator()
 graph, constrain_dict = gen.get_standard_set()[6]
@@ -38,38 +39,46 @@ hoppa = SimulateSquatHop(sqh_p)
 
 
 q_act, vq_act, acc_act, tau = hoppa.simulate(
-    robo_urdf, joint_description, loop_description, is_vis=True)
+    robo_urdf, joint_description, loop_description, is_vis=False)
 
 trj_f = hoppa.create_traj_equation()
 t = np.linspace(0, sqh_p.total_time,  len(q_act))
 list__234 = np.array(list(map(trj_f, t)))
 
-
-plt.figure()
-plt.plot(list__234[:, 2])
-plt.title("Desired acceleration")
-plt.xlabel("Time")
-plt.ylabel("Z-acc")
-plt.grid(True)
-
-plt.figure()
-plt.plot(tau[:, 0])
-plt.plot(tau[:, 1])
-plt.title("Actual torques")
-plt.xlabel("Time")
-plt.ylabel("Torques")
-plt.grid(True)
+trg1 = np.sin(80*0.001*t)
+trg2 = np.sin(100*0.001*t)
+double_sin = np.column_stack((trg1, trg2))
+сomparison_ratio_positive_torques(tau, double_sin)
+max_toque_ratio(tau, double_sin)
+pass
+print("FFF")
 
 
-plt.figure()
+# plt.figure()
+# plt.plot(list__234[:, 2])
+# plt.title("Desired acceleration")
+# plt.xlabel("Time")
+# plt.ylabel("Z-acc")
+# plt.grid(True)
 
-plt.plot(vq_act[:, 0])
-plt.plot(list__234[:, 1])
-plt.title("Velocities")
-plt.xlabel("Time")
-plt.ylabel("Z-vel")
-plt.legend(["actual vel", "desired vel"])
-plt.grid(True)
+# plt.figure()
+# plt.plot(tau[:, 0])
+# plt.plot(tau[:, 1])
+# plt.title("Actual torques")
+# plt.xlabel("Time")
+# plt.ylabel("Torques")
+# plt.grid(True)
 
-plt.show()
+
+# plt.figure()
+
+# plt.plot(vq_act[:, 0])
+# plt.plot(list__234[:, 1])
+# plt.title("Velocities")
+# plt.xlabel("Time")
+# plt.ylabel("Z-vel")
+# plt.legend(["actual vel", "desired vel"])
+# plt.grid(True)
+
+# plt.show()
 pass
--- auto_robot_design/pinokla/simulation_compare.py
+++ auto_robot_design/pinokla/simulation_compare.py
@@ -11,19 +11,30 @@ from auto_robot_design.description.utils import (
 from auto_robot_design.generator.restricted_generator.two_link_generator import TwoLinkGenerator
 from auto_robot_design.pinokla.squat import SquatHopParameters, SimulateSquatHop
 
-
 from typing import overload
 
 
-def compare_powers_torque(tau_taj_1, tau_traj_2):
-    # Вычисляем сумму abs() по траекториям. Чтобы дальнейшее 
-    # вычитание было иммунно к смене главного двигателя
-    # Делим на ссумму чтобы узнать процент
-    # Выводим ввиде графика и среденго значния улучшения
-    pass
-def max_toque(tau_taj_1, tau_traj_2):
-    # Максимальный момент в абсолютных значениях
-    pass
-def calculate_cooperation(tau_taj_1, tau_traj_2):
-    # Вычисляем разницу в моментах на движках
-    pass
+def сomparison_ratio_positive_torques(tau_traj_1: np.ndarray, tau_traj_2: np.ndarray):
+    abs_1 = np.abs(tau_traj_1)
+    abs_1_max = np.max(abs_1, axis=1)
+    abs_2 = np.abs(tau_traj_2)
+    abs_2_max = np.max(abs_2, axis=1)
+    divided = np.divide(abs_1_max,
+                        abs_2_max,
+                        out=1000*np.ones_like(abs_2_max),
+                        where=abs_2_max > 0.00001)
+
+    return divided
+
+def max_toque_ratio(tau_traj_1: np.ndarray, tau_traj_2: np.ndarray):
+    abs_1 = np.abs(tau_traj_1)
+    abs_2 = np.abs(tau_traj_2)
+    TOP_PERCENT = 0.15
+    top_samples_number = int(len(tau_traj_1)*TOP_PERCENT)
+    
+    abs_1_top = np.sort(abs_1, axis=0)[-top_samples_number:]
+    abs_2_top = np.sort(abs_2, axis=0)[-top_samples_number:]
+    abs_1_top_mean = np.mean(abs_1_top, axis=0)
+    abs_2_top_mean = np.mean(abs_2_top, axis=0)
+
+    return abs_1_top_mean, abs_2_top_mean 
