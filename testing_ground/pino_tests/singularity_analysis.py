0521fbcae0e0e91b6a733531f8d10c0eeda2f683
KirillZharkov <labradudel13@gmail.com>
2024-05-23 14:11:06 +0300

    move file

--- testing_ground/pino_tests/singularity_analysis.py
+++ testing_ground/pino_tests/singularity_analysis.py
@@ -25,7 +25,7 @@ from auto_robot_design.description.builder import ParametrizedBuilder, DetailedU
 
 generator = TwoLinkGenerator()
 all_graphs = generator.get_standard_set(shift=0.3)
-graph, constrain_dict = all_graphs[1]
+graph, constrain_dict = all_graphs[0]
 
 actuator = TMotor_AK10_9()
 thickness = 0.04
@@ -35,24 +35,22 @@ builder = ParametrizedBuilder(
     actuator=actuator,
     thickness=thickness)
 
-# 2) characteristics to be calculated
-# criteria that either calculated without any reference to points, or calculated through the aggregation of values from all points on trajectory
+
 dict_trajectory_criteria = {"MASS": NeutralPoseMass()}
-# criteria calculated for each point on the trajectory
+
 dict_point_criteria = {
     "Effective_Inertia": EffectiveInertiaCompute(),
     "Actuated_Mass": ActuatedMass(),
     "Manip_Jacobian": ManipJacobian(MovmentSurface.XZ)
 }
-# special object that calculates the criteria for a robot and a trajectory
+
 crag = CriteriaAggregator(dict_point_criteria, dict_trajectory_criteria)
 
-# the result is the dict with key - joint_point, value - tuple of all possible coordinate moves
 optimizing_joints = get_optimizing_joints(graph, constrain_dict)
 ground_symmetric_step = convert_x_y_to_6d_traj_xz(
-    *create_simple_step_trajectory(starting_point=[-0.15, -1.0],
-                                   step_height=0.5,
-                                   step_width=1,
+    *create_simple_step_trajectory(starting_point=[-0.5, -1.0],
+                                   step_height=0.3,
+                                   step_width=0.7,
                                    n_points=150))
 
 acceleration_capability = AccelerationCapability(
@@ -61,8 +59,9 @@ acceleration_capability = AccelerationCapability(
     error_key="error",
     actuated_mass_key="Actuated_Mass")
 
-# set up special classes for reward calculations
-error_calculator = PositioningErrorCalculator(error_key='error')
+
+error_calculator = PositioningErrorCalculator(
+    error_key='error', jacobian_key="Manip_Jacobian")
 soft_constrain = PositioningConstrain(error_calculator=error_calculator,
                                       points=[ground_symmetric_step])
 reward_manager = RewardManager(crag=crag)
@@ -79,19 +78,22 @@ plt.figure()
 for _, trajectory in reward_manager.trajectories.items():
     plt.plot(trajectory[:, 0], trajectory[:, 2])
 
-#plt.plot(workspace_trajectory[:, 0], workspace_trajectory[:, 2])
 
-
-constrain_error, results, pos_err = soft_constrain.calculate_constrain_error(
+total_error, results = soft_constrain.calculate_constrain_error(
     reward_manager.crag, fixed_robot, free_robot)
+
+
+print(f"Constrain error {total_error}")
+isotropic_values = error_calculator.calculate_isotropic_values(results[0][0])
+ 
+
 plt.figure()
-plt.title("Egg val")
-plt.plot(np.array(constrain_error)[0])
-#plt.plot(np.array(constrain_error)[0][:,1])
+plt.title("Isotropic value")
+plt.plot(isotropic_values)
+
 
 plt.figure()
 plt.title("Pos err")
-plt.plot(pos_err)
+plt.plot(results[0][2]["error"])
 
 plt.show()
-print(constrain_error)
