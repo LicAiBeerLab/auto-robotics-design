bd110e83c4195be180f5c522ae4e90ea6683e42e
Mikhail Chaikovskii <chaykovskiym.e@gmail.com>
2025-07-25 11:33:19 +0300

    patches for triangle links

--- auto_robot_design/description/utils.py
+++ auto_robot_design/description/utils.py
@@ -238,6 +238,14 @@ def draw_joint_point(graph: nx.Graph, labels=0, draw_legend=True, draw_lines=Fal
                 fillstyle="none", label="Active")
 
     if draw_legend: plt.legend()
+    if "patches_list" in kwargs:
+        patches_list = kwargs["patches_list"]
+        for patch_labels in patches_list:
+            vertices = []
+            for i, pos in enumerate(pos_list):
+                if i in patch_labels[0]: vertices.append(pos)
+            triangle = patches.Polygon(vertices, closed=True, edgecolor='black', facecolor=patch_labels[1])
+            plt.gca().add_patch(triangle)
 
 
 def draw_joint_point_widjet(graph: nx.Graph, labels=0, draw_legend=True, draw_lines=False, patches_list=[]):
